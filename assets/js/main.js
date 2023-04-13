bbaab7d423017b1966d95493d3079f60042414e5
OksanaKarpyn <93349703+OksanaKarpyn@users.noreply.github.com>
2023-04-13 14:36:14 +0200

    add una function item

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,12 +20,39 @@ const images = [{
     text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
 }];
 
-
+let imgGen = document.querySelector('.general-img');
+let thumb = document.querySelector('.thumbnails');
 for (let i = 0; i < images.length; i++) {
     let singleElements = images[i];
     console.log(singleElements);
     let pathImg = './assets/img/'
     let img = `${pathImg}${singleElements.image}`
-    let title =`${singleElements.title}`
-    let text=`${singleElements.text}`
+    let title = `${singleElements.title}`
+    let text = `${singleElements.text}`
+    item(img,title,text)
+}
+
+
+function item(img, title,text) {
+    // img
+    let imgItem = document.createElement('img')
+    imgItem.src = img
+    imgItem.className = 'item'
+    imgItem.className = 'item.active'
+    imgItem.classList.remove('display:none')
+    imgGen.appendChild(imgItem)
+    // title
+    let divTitle = document.createElement('div')
+    imgItem.appendChild(divTitle)
+    // h2
+    let h2Title = document.createElement('h2')
+    h2Title.innerHTML+= title;
+    divTitle.appendChild(h2Title);
+    // p
+    let p = document.createElement('p')
+    p.innerHTML+=text
+    divTitle.appendChild(p)
+}
+function tumbnails(){
+    let thumbImg
 }
